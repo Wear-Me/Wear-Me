1da14d077469fa628416c289e648f8bbc9be7d22
InTaek <dlsxor21c@naver.com>
2018-06-29 08:42:25 +0900

    refactor: Add code readability

--- kk/src/main/ConceptSelection.java
+++ kk/src/main/ConceptSelection.java
@@ -48,62 +48,55 @@ public class ConceptSelection extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
-		//전체 버튼
+
+		// 전체 버튼
 		JButton btnAll = new JButton("");
 		btnAll.setIcon(new ImageIcon("C:\\Users\\BYE\\Desktop\\100예은\\사딘\\폰배경\\001.jpg"));
 		btnAll.setBounds(45, 137, 95, 95);
 		contentPane.add(btnAll);
-		//버튼 투명화
+		// 버튼 투명화
 		btnAll.setBorderPainted(false);
 		btnAll.setContentAreaFilled(false);
-		
-		
-		//데이트 버튼
+
+		// 데이트 버튼
 		JButton btnDate = new JButton("");
 		btnDate.setIcon(new ImageIcon("C:\\Users\\BYE\\Desktop\\100예은\\사딘\\폰배경\\003.jpg"));
 		btnDate.setBounds(221, 137, 95, 95);
 		contentPane.add(btnDate);
 		btnDate.setBorderPainted(false);
 		btnDate.setContentAreaFilled(false);
-		
-		
-		//홍대 버튼
+
+		// 홍대 버튼
 		JButton btnHongik = new JButton("New button");
 		btnHongik.setBounds(45, 293, 95, 95);
 		contentPane.add(btnHongik);
 		btnHongik.setBorderPainted(false);
 		btnHongik.setContentAreaFilled(false);
-		
-		
-		//강남 버튼
+
+		// 강남 버튼
 		JButton btnGangnam = new JButton("New button");
 		btnGangnam.setBounds(221, 293, 95, 95);
 		contentPane.add(btnGangnam);
 		btnGangnam.setBorderPainted(false);
 		btnGangnam.setContentAreaFilled(false);
 
-		
-		//해커톤 버튼
+		// 해커톤 버튼
 		JButton btnHack = new JButton("New button");
 		btnHack.setBounds(45, 454, 95, 95);
 		contentPane.add(btnHack);
 		btnHack.setBorderPainted(false);
 		btnHack.setContentAreaFilled(false);
-		
-		
-		//면접 버튼
+
+		// 면접 버튼
 		JButton btnInterv = new JButton("New button");
 		btnInterv.setBounds(221, 454, 95, 95);
 		contentPane.add(btnInterv);
 		btnInterv.setBorderPainted(false);
 		btnInterv.setContentAreaFilled(false);
-		
+
 		JLabel label = new JLabel("원하는 스타일을 선택해봐");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setBounds(60, 50, 215, 32);
 		contentPane.add(label);
-		
 	}
 }
