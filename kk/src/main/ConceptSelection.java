5a2972b39205cfe30d749a722eabcd96c7eb0dcd
InTaek <dlsxor21c@naver.com>
2018-06-29 09:00:29 +0900

    feat: Add button & actionlistener

--- kk/src/main/ConceptSelection.java
+++ kk/src/main/ConceptSelection.java
@@ -17,9 +17,16 @@ import javax.swing.JTextPane;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 
-public class ConceptSelection extends JFrame {
+public class ConceptSelection extends JFrame implements ActionListener {
 
-	private JPanel contentPane;
+	private JPanel conceptPane; // 컨셉을 고르기 위한 panel
+	JButton btnAll = new JButton("전체");
+	JButton btnDate = new JButton("데이트");
+	JButton btnHongik = new JButton("홍대");
+	JButton btnGangnam = new JButton("강남");
+	JButton btnHack = new JButton("해커톤");
+	JButton btnInterv = new JButton("인터뷰");
+	JLabel label = new JLabel("원하는 스타일을 선택해봐");
 
 	/**
 	 * Launch the application.
@@ -43,60 +50,78 @@ public class ConceptSelection extends JFrame {
 	public ConceptSelection() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 362, 625);
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		conceptPane = new JPanel();
+		conceptPane.setBackground(Color.WHITE);
+		conceptPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(conceptPane);
+		conceptPane.setLayout(null);
 
 		// 전체 버튼
-		JButton btnAll = new JButton("");
 		btnAll.setIcon(new ImageIcon("C:\\Users\\BYE\\Desktop\\100예은\\사딘\\폰배경\\001.jpg"));
 		btnAll.setBounds(45, 137, 95, 95);
-		contentPane.add(btnAll);
+		conceptPane.add(btnAll);
 		// 버튼 투명화
 		btnAll.setBorderPainted(false);
 		btnAll.setContentAreaFilled(false);
 
 		// 데이트 버튼
-		JButton btnDate = new JButton("");
 		btnDate.setIcon(new ImageIcon("C:\\Users\\BYE\\Desktop\\100예은\\사딘\\폰배경\\003.jpg"));
 		btnDate.setBounds(221, 137, 95, 95);
-		contentPane.add(btnDate);
+		conceptPane.add(btnDate);
 		btnDate.setBorderPainted(false);
 		btnDate.setContentAreaFilled(false);
 
 		// 홍대 버튼
-		JButton btnHongik = new JButton("New button");
 		btnHongik.setBounds(45, 293, 95, 95);
-		contentPane.add(btnHongik);
+		conceptPane.add(btnHongik);
 		btnHongik.setBorderPainted(false);
 		btnHongik.setContentAreaFilled(false);
 
 		// 강남 버튼
-		JButton btnGangnam = new JButton("New button");
 		btnGangnam.setBounds(221, 293, 95, 95);
-		contentPane.add(btnGangnam);
+		conceptPane.add(btnGangnam);
 		btnGangnam.setBorderPainted(false);
 		btnGangnam.setContentAreaFilled(false);
 
 		// 해커톤 버튼
-		JButton btnHack = new JButton("New button");
 		btnHack.setBounds(45, 454, 95, 95);
-		contentPane.add(btnHack);
+		conceptPane.add(btnHack);
 		btnHack.setBorderPainted(false);
 		btnHack.setContentAreaFilled(false);
 
 		// 면접 버튼
-		JButton btnInterv = new JButton("New button");
 		btnInterv.setBounds(221, 454, 95, 95);
-		contentPane.add(btnInterv);
+		conceptPane.add(btnInterv);
 		btnInterv.setBorderPainted(false);
 		btnInterv.setContentAreaFilled(false);
 
-		JLabel label = new JLabel("원하는 스타일을 선택해봐");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setBounds(60, 50, 215, 32);
-		contentPane.add(label);
+		conceptPane.add(label);
+
+		btnAll.addActionListener(this);
+		btnDate.addActionListener(this);
+		btnHongik.addActionListener(this);
+		btnGangnam.addActionListener(this);
+		btnHack.addActionListener(this);
+		btnInterv.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO 자동 생성된 메소드 스텁
+		if (e.getSource() == btnAll) {
+
+		} else if (e.getSource() == btnDate) {
+
+		} else if (e.getSource() == btnHongik) {
+
+		} else if (e.getSource() == btnGangnam) {
+
+		} else if (e.getSource() == btnHack) {
+
+		} else if (e.getSource() == btnInterv) {
+
+		}
 	}
 }
