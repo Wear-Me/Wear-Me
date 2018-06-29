44e4dbf472b8c27720d090883695523742f8bf42
Sangwon Hong <qpakzk@gmail.com>
2018-06-29 22:08:49 +0900

    Hongik: Refactoring HongConcept.java
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- kk/src/main/HongConcept.java
+++ kk/src/main/HongConcept.java
@@ -1,27 +1,40 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-public class HongConcept extends JFrame {
+public class HongConcept extends JFrame implements ActionListener {
 	// 11~15
 	File[] list;
 	File path;
-	public ImageIcon img1;
-	public ImageIcon img2;
-	public ImageIcon img3;
-	private JLabel lblNewLabel;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	
+	JLabel label = new JLabel("홍대");
+	
+	JLabel item1 = new JLabel("홍대 1");
+	JLabel item2 = new JLabel("홍대 2");
+	JLabel item3 = new JLabel("홍대 3");
+	
+	JButton btn1;
+	JButton btn2;
+	JButton btn3;
+	
+	JButton backBtn = new JButton();
+	
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
 
 	public HongConcept() {
 		Render();
@@ -30,55 +43,80 @@ public class HongConcept extends JFrame {
 	public void Render() {
 		ImageSave();
 
-		setSize(360, 652);
+		btn1 = new JButton(new ImageIcon("./image//Style//" + list[11].getName()));
+		btn2 = new JButton(new ImageIcon("./image//Style//" + list[12].getName()));
+		btn3 = new JButton(new ImageIcon("./image//Style//" + list[13].getName()));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 360, 625);
+		panel.setBackground(Color.WHITE);
+		panel.setLayout(null);
+		
+		btn1.setBounds(20, 100, 130, 130);
+		panel.add(btn1);
+		btn1.setBorderPainted(false);
+		btn1.setContentAreaFilled(false);
+		
+		btn2.setBounds(20, 270, 130, 130);
+		panel.add(btn2);
+		btn2.setBorderPainted(false);
+		btn2.setContentAreaFilled(false);
+		
+		btn3.setBounds(20, 440, 130, 130);
+		panel.add(btn3);
+		btn3.setBorderPainted(false);
+		btn3.setContentAreaFilled(false);
+		
+		backBtn.setBounds(30, 50, 20, 40);
+		panel.add(backBtn);
 
-		this.img1 = new ImageIcon("./image//Style//" + list[11].getName());
-		this.img2 = new ImageIcon("./image//Style//" + list[12].getName());
-		this.img3 = new ImageIcon("./image//Style//" + list[13].getName());
+		item1.setHorizontalAlignment(SwingConstants.CENTER);
+		item1.setBounds(200, 120, 100, 100);
+		panel.add(item1);
 
-		getContentPane().setLayout(null);
+		item2.setHorizontalAlignment(SwingConstants.CENTER);
+		item2.setBounds(200, 290, 100, 100);
+		panel.add(item2);
 
-		lblNewLabel = new JLabel(img1);// ����
-		lblNewLabel.setBounds(148, 15, 173, 157);
-		getContentPane().add(lblNewLabel);
+		item3.setHorizontalAlignment(SwingConstants.CENTER);
+		item3.setBounds(200, 460, 100, 100);
+		panel.add(item3);
+		
+		
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setBounds(60, 50, 215, 32);
+		panel.add(label);
 
-		JLabel label = new JLabel(img2);// �߰�
-		label.setBounds(148, 199, 173, 157);
-		getContentPane().add(label);
-		String str;
-		JLabel label_1 = new JLabel(img3);// �Ʒ�
-		label_1.setBounds(148, 395, 173, 157);
-		getContentPane().add(label_1);
-		int idx = 0;
-		str = list[0].getName();
-		idx = str.lastIndexOf(".");
+		frame.getContentPane().add(panel);
+		frame.setVisible(true);
 
-		textField = new JTextField(str);// ����
-		textField.setBounds(0, 105, 130, 27);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-
-		str = list[1].getName();
-		idx = str.lastIndexOf(".");
-
-		textField_1 = new JTextField(str);// �߰�
-		textField_1.setColumns(10);
-		textField_1.setBounds(0, 223, 131, 27);
-		getContentPane().add(textField_1);
-
-		str = list[2].getName();
-		idx = str.lastIndexOf(".");
-
-		textField_2 = new JTextField(str);// �Ʒ�
-		textField_2.setColumns(10);
-		textField_2.setBounds(0, 329, 130, 27);
-		getContentPane().add(textField_2);
-		setVisible(true);
+		btn1.addActionListener(this);
+		btn2.addActionListener(this);
+		btn3.addActionListener(this);
+		backBtn.addActionListener(this);
 	}
 
 	void ImageSave() {
 		path = new File("./image//Style//");
 		list = path.listFiles();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO 자동 생성된 메소드 스텁
+		if (e.getSource() == btn1) {
+			frame.setVisible(false);
+			new BodyType();
+		} else if (e.getSource() == btn2) {
+			frame.setVisible(false);
+			new BodyType();
+		} else if (e.getSource() == btn3) {
+			frame.setVisible(false);
+			new BodyType();
+		} else if (e.getSource() == backBtn) {
+			frame.setVisible(false);
+			new ConceptSelection();
+		}
+
 	}
 
 }
